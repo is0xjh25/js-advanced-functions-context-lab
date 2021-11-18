c998328494a1a858744e9a2395bc875674b33b41
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-18 11:56:59 +0800

    Update: Test not passed

--- index.js
+++ index.js
@@ -9,6 +9,63 @@
  for you to use if you need it!
  */
 
+ // Your code here
+function createEmployeeRecord(row) {
+	return {
+		firstName: row[0],
+		familyName: row[1],
+		title: row[2],
+		payPerHour: row[3],
+		timeInEvents:[],
+		timeOutEvents:[],
+	}
+}
+
+function createEmployeeRecords(arr) {
+	
+	return arr.map(employee => {
+		return createEmployeeRecord(employee);
+	})
+}
+
+function createTimeInEvent(datestamp) {
+	const [date, hour] = datestamp.split(" ");        
+	this.timeInEvents.push({type: "TimeIn", hour: parseInt(hour, 10), date});
+	console.log(this.timeInEvents[0]); 
+    return this.timeInEvents;
+}
+
+function createTimeOutEvent(datestamp) {
+	const [date, hour] = datestamp.split(" ");        
+	this.timeOutEvents.push({type: "TimeOut", hour: parseInt(hour, 10), date});
+	return this.timeOutEvents;   
+}
+
+function hoursWorkedOnDate(date) {
+	let timeIn = this.timeInEvents.filter(e => e.date === date);
+	let timeOut = this.timeOutEvents.filter(e => e.date === date);
+	return parseInt(timeOut[0].hour - timeIn[0].hour) / 100;
+}
+
+function wagesEarnedOnDate(em, date) {
+	let hours = hoursWorkedOnDate.call(this, em, date);
+	return hours * em.payPerHour;
+}
+
+function calculatePayroll(ems) {
+	return ems.map(em => allWagesFor(em))
+	.reduce((current, next) => current + next);
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+	srcArray.forEach(emp => {
+		if (emp.firstName === firstName) {
+			return emp.familyName;
+		}
+	});
+	return srcArray[0];
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
